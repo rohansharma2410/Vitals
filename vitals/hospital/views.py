125903541382713c7d8f535bc7b661c2dcb98cc0
Rohan Sharma <sharmar2410@gmail.com>
2023-05-23 00:00:37 +0530

    f
    Signed-off-by: Rohan Sharma <sharmar2410@gmail.com>

--- vitals/hospital/views.py
+++ vitals/hospital/views.py
@@ -842,7 +842,3 @@ def contactus_view(request):
 #---------------------------------------------------------------------------------
 
 
-
-#Developed By : sumit kumar
-#facebook : fb.com/sumit.luv
-#Youtube :youtube.com/lazycoders
